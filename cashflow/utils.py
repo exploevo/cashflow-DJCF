4881d71f526406777484c7ed6b5921567334a2d7
exploevo <sharingevolving@gmail.com>
2022-06-22 07:14:50 +0200

    move xmltodict pd.DataFrame to process_xml_file

--- cashflow/utils.py
+++ cashflow/utils.py
@@ -5,14 +5,14 @@ from pathlib import Path
 
 def process_xml_file(file):
     xml_text = Path(file.path).read_text()
-    client_data = get_client_from_xml(xml_text)
-    supplier_data = get_supplier_from_xml(xml_text)
-    invoice_data = get_invoice_from_xml(xml_text)
-    return (client_data, supplier_data, invoice_data)
-
-def get_client_from_xml(xml_text):
     obj = xmltodict.parse(xml_text)
     df = pd.DataFrame(obj)
+    client_data = get_client_from_xml(df)
+    supplier_data = get_supplier_from_xml(df)
+    invoice_data = get_invoice_from_xml(df)
+    return (client_data, supplier_data, invoice_data)
+
+def get_client_from_xml(df):
     iva = df['p:FatturaElettronica']['FatturaElettronicaHeader']['CessionarioCommittente']['DatiAnagrafici']['IdFiscaleIVA']['IdCodice']
     fis = df['p:FatturaElettronica']['FatturaElettronicaHeader']['CessionarioCommittente']['DatiAnagrafici']['CodiceFiscale']
     try: 
@@ -33,9 +33,7 @@ def get_client_from_xml(xml_text):
         'last_name': last_name,
     }
 
-def get_supplier_from_xml(xml_text):
-    obj = xmltodict.parse(xml_text)
-    df = pd.DataFrame(obj)
+def get_supplier_from_xml(df):
     iva = df['p:FatturaElettronica']['FatturaElettronicaHeader']['CedentePrestatore']['DatiAnagrafici']['IdFiscaleIVA']['IdCodice']
     fis = df['p:FatturaElettronica']['FatturaElettronicaHeader']['CedentePrestatore']['DatiAnagrafici']['CodiceFiscale']
     try: 
@@ -56,9 +54,7 @@ def get_supplier_from_xml(xml_text):
         'last_name': last_name,
     }
 
-def get_invoice_from_xml(xml_text):
-    obj = xmltodict.parse(xml_text)
-    df = pd.DataFrame(obj)
+def get_invoice_from_xml(df):
     doc_num = df['p:FatturaElettronica']['FatturaElettronicaBody']['DatiGenerali']['DatiGeneraliDocumento']['Numero']
     payment_cond = df['p:FatturaElettronica']['FatturaElettronicaBody']['DatiPagamento']['CondizioniPagamento']
     payment_mod = df['p:FatturaElettronica']['FatturaElettronicaBody']['DatiPagamento']['DettaglioPagamento']['ModalitaPagamento']
