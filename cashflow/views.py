e139c6d230e01fbdad15f8b167e063b1e82738c6
exploevo <sharingevolving@gmail.com>
2022-06-30 07:35:17 +0200

    Created Register form with added fields piva and image

--- cashflow/views.py
+++ cashflow/views.py
@@ -1,4 +1,5 @@
 
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 # from django.views import generic
 from django.contrib.auth import authenticate, login
@@ -39,7 +40,8 @@ def edit(request):
             user_form.save()
             profile_form.save()
             messages.success(request, "Data Successfully Updated!")
-        #I would like to reload the page in order to see the image    
+        #I would like to reload the page in order to see the image
+        # Also the piva must be ad obliged field in order to be a valid user    
     else:
         user_form = UserEditForm(instance = request.user)
         profile_form = ProfileEditForm(instance = request.user.profile)
@@ -51,6 +53,7 @@ def edit(request):
 def register(request):
     if request.method == 'POST':
         user_form = UserRegistrationForm(request.POST)
+        pform = ProfileEditForm(data = request.POST)
         if user_form.is_valid():
             # Create a new user object but avoid saving it yet
             new_user = user_form.save(commit=False)
@@ -59,16 +62,23 @@ def register(request):
                 user_form.cleaned_data['password'])
             # Save the User object
             new_user.save()
+            profile_form = pform.save(commit = False)
             #create the user profile
-            Profile.objects.create(user=new_user)
+            #Profile.objects.create(user=new_user)
+            messages.success(request, f"{new_user.id}")
+            profile_form.user = new_user
+            profile_form.save()
+            messages.success(request, "Profile Successfully Created!")
             return render(request,
                           'cashflow/register_done.html',
                           {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
+        profile_form = ProfileEditForm()
     return render(request,
                   'cashflow/register.html',
-                  {'user_form': user_form})
+                  {'user_form': user_form,
+                  'profile_form' : profile_form})
 
 def add_xml_file(request):
     if request.method == 'POST':
